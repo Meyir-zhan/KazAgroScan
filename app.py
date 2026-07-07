0785379783bb9552b3ff7e9f04e61a03ed16649b
Meyir-zhan <251994@astanait.edu.kz>
2026-07-07 20:18:40 +0500

    Force push update to app.py

--- app.py
+++ app.py
@@ -7,14 +7,26 @@ date = st.sidebar.date_input('Select the date')
 coordinates = st.sidebar.text_input('Enter coords as: lat, lon, lat, lon') 
 
 @st.cache_data
-def get_data_from_api(date, coordinates):
-
-    fetch = fetcher.DataFetcher(coordinates)
+def get_data_from_api(date, geometry):
+    fetch = fetcher.DataFetcher(geometry)
     red, nir = fetch.fetch_satalite_data(date)
     return red, nir
 
 if st.button('Analyze Crop'):
-    red, nir = get_data_from_api(date,coordinates)
+    if coordinates:
+        coords = [float(x.strip()) for x in coordinates.split(',')]
+        
+       
+        points = [[coords[i], coords[i+1]] for i in range(0, len(coords), 2)]
+        
+        
+        geometry = {
+            "type": "Polygon",
+            "coordinates": [points]
+        }
+        
+        
+        red, nir = get_data_from_api(date, geometry)
 
     analyzer_instance = analyzer.DataAnalayzer(red, nir)
     ndvi = analyzer_instance.calculate_ndvi()
